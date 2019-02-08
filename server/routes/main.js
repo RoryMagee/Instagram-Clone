e5938fd3174874b3f4413de609a65d4079d5ecc9
Rory Magee <rory.magee2@gmail.com>
2019-02-08 18:57:09 +0000

    added login route

--- server/routes/main.js
+++ server/routes/main.js
@@ -31,6 +31,32 @@ router.post('/signup', (req, res, next) => {
     });
 });
 
+router.post('/login', (req, res, next) => {
+    User.findOne({email: req.body.email}, (err, user) => {
+        if (err) {
+            res.json({
+                success: false,
+                message: "No account registered with that email address"
+            });
+        } else if (user) {
+            let validPassword =  user.comparePassword(req.body.password);
+            if(!validPassword) {
+                res.json({
+                    success: false,
+                    message: 'Auth Failed'
+                });
+            } else {
+                let token = jwt.sign({user: user}, process.env.secret, {expiresIn: '7d'});
+                res.json({
+                    success: true,
+                    message: "Login Successful",
+                    token: token
+                });
+            }
+        }
+    });
+});
+
 //Temp route for deleting all account
 router.delete('/signup', (req, res, next) => {
     User.remove({}, (err) => {
